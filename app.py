3808bb62f6166dae480f8e99186ad8238681c1d3
matteo <matteomontresor91@gmail.com>
2024-05-12 17:19:27 +0200

    mise a jour

--- app.py
+++ app.py
@@ -47,10 +47,9 @@ def delete(todo_id):
 
 
 
+with app.app_context():
+    db.create_all()
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
-    with app.app_context():
-        db.create_all()
-        app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
