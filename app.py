110732a006e9bbfb8dc818cf8914f93333c32ca3
leroy97one <matteomontresor91@gmail.com>
2024-08-07 04:08:07 -0700

    test

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ from datetime import datetime
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://myuser:mypassword@db:5432/mydatabase'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app) 
-app.config['SQLALCHEMY_ENGINE_OPTIONS'] =  {
+db = SQLAlchemy(app)
+app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {
     'pool_pre_ping': True,
 }
 
@@ -17,42 +17,45 @@ class Todo(db.Model):
 
 @app.route("/")
 def index():
-    todo_list = Todo.query.all()
-    print(todo_list)
-    return render_template("index.html", todo_list =todo_list)
-
-
+    try:
+        todo_list = Todo.query.all()
+        return render_template("index.html", todo_list=todo_list)
+    except Exception as e:
+        return str(e), 500
 
 @app.route("/add", methods=["POST"])
 def add():
-    title = request.form.get("title")
-    new_todo = Todo(title=title, complete=False)
-    db.session.add(new_todo)
-    db.session.commit()
-    return redirect(url_for("index"))
+    try:
+        title = request.form.get("title")
+        new_todo = Todo(title=title, complete=False)
+        db.session.add(new_todo)
+        db.session.commit()
+        return redirect(url_for("index"))
+    except Exception as e:
+        return str(e), 500
 
 @app.route("/update/<int:todo_id>")
 def update(todo_id):
-    todo = Todo.query.filter_by(id=todo_id).first()
-    todo.complete = not todo.complete
-    db.session.commit()
-    return redirect(url_for("index"))
-
+    try:
+        todo = Todo.query.filter_by(id=todo_id).first()
+        todo.complete = not todo.complete
+        db.session.commit()
+        return redirect(url_for("index"))
+    except Exception as e:
+        return str(e), 500
 
 @app.route("/delete/<int:todo_id>")
 def delete(todo_id):
-
-    todo = Todo.query.filter_by(id=todo_id).first()
-    db.session.delete(todo)
-    db.session.commit()
-    return redirect(url_for("index"))
-
-
-
+    try:
+        todo = Todo.query.filter_by(id=todo_id).first()
+        db.session.delete(todo)
+        db.session.commit()
+        return redirect(url_for("index"))
+    except Exception as e:
+        return str(e), 500
 
 with app.app_context():
     db.create_all()
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
